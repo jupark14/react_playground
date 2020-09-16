027e252c2dedbd3ec4948d2a72ddd88c6fcd0795
jupark14 <25th_night@kakao.com>
2020-09-16 23:28:05 +0900

    filter, concat을 배움

--- velopert/src/App.js
+++ velopert/src/App.js
@@ -1,22 +1,11 @@
 import React, { Component } from "react";
-import ScrollBox from "./ScrollBox";
-import ValidationSample from "./ValidationSample";
+import IterationSample from "./IterationSample";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ValidationSample />
-        <ScrollBox
-          ref={(ref) => {
-            this.scrollBox = ref;
-          }}
-        />
-        <button
-          onClick={() => {
-            this.scrollBox.scrollToButtom();
-          }}
-        ></button>
+        <IterationSample />
       </div>
     );
   }
